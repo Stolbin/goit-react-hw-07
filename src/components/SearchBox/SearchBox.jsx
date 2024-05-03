1bd8a8097b43b431160aaba909f4248d1e5cb2c0
Stolbin <109080579+Stolbin@users.noreply.github.com>
2024-05-03 18:37:03 +0300

    Update SearchBox.jsx

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,6 +1,6 @@
 import { useDispatch } from "react-redux";
 import css from "./SearchBox.module.css";
-import { changeFilter } from "../../../../goit-react-hw-06/src/redux/filtersSlice";
+import { changeFilter } from "../../redux/filtersSlice";
 
 const SearchBox = () => {
   const dispatch = useDispatch();
